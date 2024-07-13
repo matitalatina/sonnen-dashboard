514fd1f9991c2e102ae53625437a69a3d0933c89
Mattia Natali <matitalatina@gmail.com>
2024-07-13 22:36:46 +0200

    Add third row

--- jobs/ecoflow.rb
+++ jobs/ecoflow.rb
@@ -3,25 +3,42 @@ if !ENV['SMASHING_HASS_TOKEN'] || !ENV['SMASHING_HASS_URL']
   else
     token = ENV['SMASHING_HASS_TOKEN']
     url = ENV['SMASHING_HASS_URL']
+    data_fetch = [
+      ['ecoflow_battery_level', 'ecoflow-charge', nil],
+      ['ecoflow_total_in_power', 'ecoflow-power-in', ChartRepo.new()],
+      ['ecoflow_total_out_power', 'ecoflow-power-out', ChartRepo.new()],
+    ]
 
-    SCHEDULER.every '30s', first_in: 0 do
-      begin  
-        info_uri = URI.parse(url + "/api/states/sensor.ecoflow_battery_level")
-        info_req = Net::HTTP::Get.new(
-          info_uri,
-          'Content-Type' => 'application/json',
-          'Authorization' => 'Bearer ' + token
-        )
-        info_res = Net::HTTP.start(info_uri.hostname, info_uri.port) do |http|
-          http.request(info_req)
+    SCHEDULER.every '10s', first_in: 0 do
+      begin
+        data_fetch.each do |entity, event, chart|
+          info_uri = URI.parse(url + "/api/states/sensor.#{entity}")
+          info_req = Net::HTTP::Get.new(
+            info_uri,
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' + token
+          )
+          info_res = Net::HTTP.start(info_uri.hostname, info_uri.port) do |http|
+            http.request(info_req)
+          end
+          info_json = JSON.parse(info_res.body)
+          print info_json
+          value = info_json['state'].to_i
+          print value
+          if not chart
+            send_event(event, { 
+              value: value,
+            })
+          else
+            chart.add(value)
+            send_event(event, {
+              points: chart.history,
+              displayed_value: chart.history.first['y']
+            })
+          end
+        rescue => err
+          puts err
         end
-        info_json = JSON.parse(info_res.body)
-        charge = info_json['state'].to_i
-        send_event('ecoflow-charge', { 
-          value: charge,
-        })
-      rescue => err
-        puts err
       end
     end
   end
